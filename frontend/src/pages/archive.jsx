98ecb6ea8eb1a9f22e32907a67ac17f2967af3e2
Natouille04 <nathan.gouveia2008@gmail.com>
2025-05-24 00:02:52 +0200

    Added archive research

--- frontend/src/pages/archive.jsx
+++ frontend/src/pages/archive.jsx
@@ -2,10 +2,29 @@ import { useState, useEffect } from 'react';
 import { Button } from '../components/button.jsx';
 import { Card } from '../components/card.jsx';
 
+function DisplayDocument(data, filter = null) {
+    if (filter === null) {
+        return (
+            data.map((item, index) => (
+                <Card key={index} title={item.title} image={item.image} id={item.id} />
+            ))
+        );
+    }
+
+    else {
+        return (
+            data.filter((item) => item.title.toLowerCase().includes(filter.toLowerCase())).map((item, index) => (
+                <Card key={index} title={item.title} image={item.image} id={item.id} />
+            ))
+        );
+    }
+}
+
 function Archive() {
     const [data, setData] = useState(null);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
+    const [search, setSearch] = useState('');
 
     useEffect(() => {
         fetch('http://localhost:8000/api/document/all')
@@ -65,18 +84,18 @@ function Archive() {
             <div className="flex flex-col justify-between h-full w-88/100">
 
                 <div className="flex flex-col h-28/100 w-full bg-[#9999FF]">
-                    <div className="w-full h-90/100 bg-black rounded-bl-3xl flex flex-row justify-between px-3 pb-5">
+                    <div className="w-full h-90/100 bg-black rounded-bl-3xl flex md:flex-row flex-col justify-between px-3 pb-5">
                         <div>
-                            <h1 className="lg:text-7xl md:text-6xl font-semibold mb-3 h-35/100">ARCHIVE</h1>
+                            <h1 className="lg:text-7xl md:text-6xl text-5xl font-semibold mb-3 h-35/100">ARCHIVE</h1>
                         </div>
 
-                        <div className="w-15/100 flex flex-col justify-between py-5">
+                        <div className="md:w-15/100 w-full md:h-auto h-50/100 flex md:flex-col flex-row justify-between py-5">
 
-                            <div className="h-45/100 flex-col text-black text-xl">
+                            <div className="md:h-45/100 h-full md:w-auto w-48/100 flex-col text-black md:text-xl text-lg">
                                 <Button text='ACCEUIL' url='/' color='#FF9900' />
                             </div>
 
-                            <div className="h-45/100 flex-col text-black text-xl">
+                            <div className="md:h-45/100 h-full md:w-auto w-48/100 flex-col text-black text-lg">
                                 <Button text='SUGGEREZ UN DOCUMENT' url='#' color='#CC6666' />
                             </div>
 
@@ -85,11 +104,13 @@ function Archive() {
                 </div>
 
                 <div className="flex flex-col justify-end h-71/100 w-full bg-[#CC6666]">
-                    <div className="w-full h-96/100 bg-black rounded-tl-3xl p-5 flex lg:flex-row flex-col items-center justify-between">
+                    <div className="w-full h-96/100 bg-black rounded-tl-3xl p-5 flex flex-col items-center justify-between">
+                        <input type='text' className='w-full h-13 border-2 p-2 text-2xl outline-0 mb-3' placeholder='RECHERCHER UN DOCUMENT' onChange={(e) => setSearch(e.target.value)}></input>
+
                         <div className="grid grid-cols-2 md:grid-cols-4 xl:grid-cols-6 gap-x-4 gap-y-6 w-full h-full overflow-y-scroll">
-                            {data.map((item, index) => (
-                                <Card key={index} title={item.title} image={item.image} id={item.id} />
-                            ))}
+                            {
+                                DisplayDocument(data, search)
+                            }
                         </div>
                     </div>
 
--- frontend/src/pages/details.jsx
+++ frontend/src/pages/details.jsx
@@ -82,43 +82,14 @@ function Details() {
             <div className="flex flex-col justify-between h-full w-88/100">
 
                 <div className="flex flex-col items-center justify-end h-60/100 w-full bg-[#9999FF]">
-                    <div className="w-full h-90/100 bg-black rounded-tl-3xl flex flex-row justify-between px-5 pt-5">
-                        <img src={data.image}></img>
-                        <div className='w-90/100 pl-5 flex flex-col'>
-                            <h1 className="text-7xl mb-2">{data.title}</h1>
-                            <p className='text-2xl my-2'>ISBN : {data.isbn}</p>
-                            <p className="w-full max-w-300 break-words whitespace-normal mb-2">{data.description}</p>
-                            <p className='text-[#C0FF89]'>{data.comment}</p>
+                    <div className="w-full h-96/100 bg-black rounded-tl-3xl flex flex-row justify-between px-5 pt-5">
 
-                            <div className='w-full border-t-2 mt-4 pt-4 flex flex-row justify-between'>
-                                <div className='w-70/100'>
-                                    <Info data={data.authors} text="Auteur(s)" />
-                                    <Info data={data.illustrators} text="Illustrateur(s)" />
-                                    <Info data={data.traductors} text="Traducteur(s)" />
-                                    <Info data={data.correctors} text="Correcteur(s)" />
-                                </div>
-
-                                <div className='w-30/100 h-70/100 grid grid-cols-2 gap-4'>
-                                    <Button text='METTRE EN FAVORIE' url='#' color='#FF9900' />
-                                    <Button text='LIRE' url='#' color='#9999FF' />
-                                    <Button text='COMMENTAIRE' url='#' color='#CC6666' />
-                                    <Button text='PARTAGEZ' url='#' color='#CC99CC' />
-                                </div>
-                            </div>
-                        </div>
                     </div>
                 </div>
 
                 <div className="flex flex-col justify-start h-40/100 w-full bg-[#FF9966]">
-                    <div className="w-full h-96/100 bg-black rounded-bl-3xl flex flex-col py-7 px-5">
-                        <p className='text-2xl'>Autres documents :</p>
-                        <div className='w-full h-full flex flex-row overflow-x-auto space-x-4 mt-5 pr-5'>
-                            {Array(20).fill(0).map((_, i) => (
-                                <div key={i} className="min-w-[200px]">
-                                    <Card id={i + 1} title='test' image={data.image} url='#' index={i} />
-                                </div>
-                            ))}
-                        </div>
+                    <div className="w-full h-95/100 bg-black rounded-bl-3xl flex flex-col py-7 px-5">
+
                     </div>
                 </div>
 
