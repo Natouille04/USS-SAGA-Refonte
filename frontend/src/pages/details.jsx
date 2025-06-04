fa906fcaef5c72d949afad97eeba4afe92d689b6
Natouille <nathan.gouveia2008@gmail.com>
2025-06-04 16:01:52 +0200

    modified details page

--- frontend/src/pages/details.jsx
+++ frontend/src/pages/details.jsx
@@ -73,7 +73,7 @@ function Details() {
                 <div className="flex flex-col h-71/100 w-full">
                     <div className="w-full h-12/25 bg-[#CC6666] mb-2"></div>
                     <div className="w-full h-3/25 bg-[#FF9900] mb-2"></div>
-                    <div className="w-full h-7/25 bg-[#FF9966] mb-2"></div>
+                    <div className="w-full h-7/25 bg-[#FF9966]"></div>
                     <div className="w-full h-3/25 bg-[#FF9966] rounded-bl-full"></div>
                 </div>
 
@@ -81,13 +81,25 @@ function Details() {
 
             <div className="flex flex-col justify-between h-full w-88/100">
 
-                <div className="flex flex-col items-center justify-end h-60/100 w-full bg-[#9999FF]">
+                <div className="flex flex-col items-center justify-end h-40/100 w-full bg-[#9999FF]">
                     <div className="w-full h-96/100 bg-black rounded-tl-3xl flex flex-row justify-between px-5 pt-5">
+                        <div className='w-full h-full flex flex-row justify-between'>
+                            <img src={data.image} alt="Document" className="w-15/100 h-full object-cover" />
 
+                            <div className='w-84/100 h-full flex flex-col'>
+                                <div className='w-full h-2/10 flex flex-row justify-between'>
+                                    <h1 className="text-5xl font-bold mt-2">{data.title}</h1>
+                                    <h2 className='text-3xl'>ISBN: {data.isbn}</h2>
+                                </div>
+
+                                <p className="text-lg mt-1 h-6/10 overflow-y-auto">{data.description}</p>
+                                <p className="text-[#C0FF89] text-lg mt-1 h-2/10 overflow-y-auto">{data.comment}</p>
+                            </div>
+                        </div>
                     </div>
                 </div>
 
-                <div className="flex flex-col justify-start h-40/100 w-full bg-[#FF9966]">
+                <div className="flex flex-col justify-start h-60/100 w-full bg-[#FF9966]">
                     <div className="w-full h-95/100 bg-black rounded-bl-3xl flex flex-col py-7 px-5">
 
                     </div>
